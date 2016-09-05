cf864748d6284ca5aa827e169a0379b3a8a2cb91
Pournami Azad <pournami@qburst.com>
2016-09-05 16:52:05 +0530

    Adding changes

--- js/new-home.js
+++ js/new-home.js
@@ -17,13 +17,13 @@ jQuery(document).ready(function(){
         jQuery('#add-test').modal('hide');
 
     });
-
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    }
-
-    function daydiff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    }
 });
+
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+}
+
+function daydiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
